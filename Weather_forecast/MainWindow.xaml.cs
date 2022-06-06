e0792fdeacc8dd2cfff16172320e7caaaa79f94e
MikolajLiszka <mikolaj.liszka@gmail.com>
2022-06-06 17:39:33 +0200

    Dodaj pliki projektów.

---
+++ Weather_forecast/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+using System.Net;
+
+namespace Weather_forecast
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void showWeatherNow_Click(object sender, RoutedEventArgs e)
+        {
+            string city;
+
+            city = citySelector.Text;
+
+            string uri = string.Format("http://api.weatherapi.com/v1/current.xml?key=48506950c6fb43ef8f7132151220606&q={0}&aqi=no", city);
+
+            XDocument doc = XDocument.Load(uri);
+
+            string iconUri = (string)doc.Descendants("imageOne").FirstOrDefault();
+
+            /*WebClient client = new WebClient();
+
+            byte[] image = client.DownloadData("http:" + iconUri);
+
+            MemoryStream stream = new MemoryStream(image);
+            */
+
+
+            string xCTemp = (string)doc.Descendants("temp_c").FirstOrDefault();
+            string xFTemp = (string)doc.Descendants("temp_f").FirstOrDefault();
+
+            string xWindDir = (string)doc.Descendants("wind_dir").FirstOrDefault();
+            string xWindSpeedKPH = (string)doc.Descendants("wind_kph").FirstOrDefault();
+
+            string xHumidity = (string)doc.Descendants("humidity").FirstOrDefault();
+
+            string xRegion = (string)doc.Descendants("region").FirstOrDefault();
+            string xCountry = (string)doc.Descendants("country").FirstOrDefault();
+
+            string xPressure = (string)doc.Descendants("pressure_mb").FirstOrDefault();
+            string xCloud = (string)doc.Descendants("cloud").FirstOrDefault();
+
+            string xVisibilityKM = (string)doc.Descendants("vis_km").FirstOrDefault();
+            string xUV = (string)doc.Descendants("uv").FirstOrDefault();
+
+            string xLastUpdate = (string)doc.Descendants("last_updated").FirstOrDefault();
+
+            celciusTemp.Text = xCTemp;
+            farenheitTemp.Text = xFTemp;
+            windDir.Text = xWindDir;
+            kphSpeed.Text = xWindSpeedKPH;
+            humidity.Text = xHumidity;
+            region.Text = xRegion;
+            country.Text = xCountry;
+            pressure.Text = xPressure;
+            cloudCover.Text = xCloud;
+            visibilityKm.Text = xVisibilityKM;
+            uv.Text = xUV;
+            lastUpdate.Text = xLastUpdate;
+        }
+    }
+}
